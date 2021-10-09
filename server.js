aba043a74431df9ed64d5d1dd6dc0cd4974fb00b
Mitchell Robles <mitchellr92@live.com>
2021-10-08 17:02:59 -0700

    created server for hfcco

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => res.json({ msg: "HFCCO API..." }));
+
+// Define routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
